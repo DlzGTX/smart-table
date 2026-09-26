87ef02bbe293d31ebf2bac1a72653fac64f7729d
Ilia Kolesnikov <ilhakolesnikow@yandex.ru>
2026-09-26 17:00:11 +0500

    first step complited

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import {initSearching} from "./components/searching.js"
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData();
 
 /**
  * Сбор и обработка полей из таблицы
@@ -37,17 +37,19 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let query = {}; // копируем для последующего изменения
     // @todo: использование
 
-    result = applySorting(result, state, action);
-    result = applyFiltering(result, state, action);
-    result = applySearching(result, state, action);
-    result = applyPagination(result, state, action);
+    query = applySorting(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySearching(query, state, action);
+    query = applyPagination(query, state, action);
 
-    sampleTable.render(result)
+    const { total, items } = await api.getRecords(query);
+
+    sampleTable.render(items)
 }
 
 const sampleTable = initTable({
@@ -75,7 +77,7 @@ const applySearching = initSearching(
 )
 
 const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-    searchBySeller: indexes.sellers                                  // для элемента с именем searchBySeller устанавливаем массив продавцов
+    searchBySeller: indexes.sellers                             // для элемента с именем searchBySeller устанавливаем массив продавцов
 }); 
 
 const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
@@ -86,5 +88,8 @@ const applySorting = initSorting([        // Нам нужно передать 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
+async function init() {
+    const indexes = await api.getIndexes();
+}
 
-render;
+init().then(render);
